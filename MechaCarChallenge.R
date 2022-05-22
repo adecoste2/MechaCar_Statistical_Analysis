1c9f08a0738ca6af7cccc316562decbf169fbb52
adecoste2 <aimeeldecoste@gmail.com>
2022-05-22 07:39:09 -0700

    "Deliverable Two MechaCarChallenge R-Script

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,4 +1,5 @@
 #Deliverable 1
+
 #load dplyr library.
 library(dplyr)
 
@@ -10,3 +11,17 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 
 # Perform summary to determine the p-value and the r-squared value for the linear regression model.
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data= MechaCar_df))
+
+# Deliverable 2
+
+# Read in the Suspension_Coil.csv file as a table.
+Suspension_Coil <- read.csv(file='Suspension_Coil.csv')
+str (Suspension_Coil)
+
+# Create a total summary DataFrame.
+total_summary<-Suspension_Coil %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = sd(PSI)*sd(PSI), SD = sd(PSI))
+total_summary
+
+# Create a lot summary DataFrame.
+lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = sd(PSI)*sd(PSI), SD = sd(PSI))
+lot_summary
